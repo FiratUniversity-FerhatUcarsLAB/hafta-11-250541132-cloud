5e748d1b04b75795d3288ef238d8654880c800e8
250541132-cloud <250541132@firat.edu.tr>
2025-12-03 21:29:19 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,5 +1,11 @@
-public class Exercise4_1 {
+/**öğrenci no:250541132
+ * öğrenci ad soyad: Esmanur ulu
+ * ödev adı:alıştırma 4.1
+ */
 
+import java.util.Scanner;
+
+public class Alistirma4_1 {
     public static void printAmerican(String day, int date, String month, int year) {
         // TODO
     }
@@ -9,6 +15,30 @@ public class Exercise4_1 {
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        // TODO: test methods // ... printAmerican ve printEuropean metotları aynı kalır ...
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Haftanın gününü giriniz (örn: Monday): ");
+        String day = scanner.nextLine();
+
+        System.out.print("Ayın adını giriniz (örn: July): ");
+        String month = scanner.nextLine();
+
+        System.out.print("Ayın tarihini giriniz (örn: 22): ");
+        int date = scanner.nextInt();
+
+        System.out.print("Yılı giriniz (örn: 2019): ");
+        int year = scanner.nextInt();
+
+        scanner.close();
+
+        System.out.println("\n--- Çıktılar ---");
+        System.out.print("Amerikan: ");
+        printAmerican(day, date, month, year);
+
+        System.out.print("Avrupa: ");
+        printEuropean(day, date, month, year);
     }
+}
 }
